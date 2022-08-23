e218c08c6e3f793c5d77b068339a289a4b165cdc
David Moore <david.moore@sas.com>
2022-08-23 17:55:27 -0400

    Cleanup Turn.java

---
+++ src/test/java/com/enigma/watatsea/events/EventDispatcherTest.java
@@ -0,0 +1,74 @@
+package com.enigma.watatsea.events;
+
+import com.enigma.waratsea.events.EventDispatcher;
+import com.enigma.waratsea.events.EventHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EventDispatcherTest {
+  private EventDispatcher<TestEvent> eventDispatcher;
+
+  class Handler implements EventHandler<TestEvent> {
+
+    @Override
+    public void notify(TestEvent event) {
+      var currentCount = event.getCount();
+      event.setCount(++currentCount);
+
+      eventDispatcher.unregister(this);
+    }
+  }
+
+  @BeforeEach
+  void setUpt() {
+    eventDispatcher = new EventDispatcher<>("TestEvent");
+  }
+
+  @Test
+  void testSingleHandlerEventDelivery() {
+    eventDispatcher.register(this::testEventHandlerOne);
+
+    var testEvent = new TestEvent(1);
+    eventDispatcher.fire(testEvent);
+
+    assertEquals(2, testEvent.getCount());
+  }
+
+  @Test
+  void testMultipleHandlerEventDelivery() {
+    eventDispatcher.register(this::testEventHandlerOne);
+    eventDispatcher.register(this::testEventHandlerTwo);
+
+    var testEvent = new TestEvent(1);
+    eventDispatcher.fire(testEvent);
+
+    assertEquals(3, testEvent.getCount());
+  }
+
+  @Test
+  void testUnregisterOnEventDelivery() {
+    eventDispatcher.register(new Handler());
+
+    var testEvent = new TestEvent(1);
+    eventDispatcher.fire(testEvent);
+
+    assertEquals(2, testEvent.getCount());
+
+    eventDispatcher.fire(testEvent);
+
+    assertEquals(2, testEvent.getCount());
+  }
+
+  private void testEventHandlerOne(final TestEvent event) {
+    var currentCount = event.getCount();
+    event.setCount(++currentCount);
+  }
+
+  private void testEventHandlerTwo(final TestEvent event) {
+    var currentCount = event.getCount();
+    event.setCount(++currentCount);
+  }
+
+}
---
+++ src/test/java/com/enigma/watatsea/events/TestEvent.java
@@ -0,0 +1,14 @@
+package com.enigma.watatsea.events;
+
+import com.enigma.waratsea.events.Event;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Strictly a class used in the aid of testing the event dispatcher.
+ */
+@Data
+@AllArgsConstructor
+public class TestEvent implements Event {
+  private int count;
+}
