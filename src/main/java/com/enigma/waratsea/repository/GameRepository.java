16a5f399696446763637ca63a1534c9d878de5de
David Moore <david.moore@sas.com>
2022-08-24 13:38:07 -0400

    Refactor events into Events.java

--- src/main/java/com/enigma/waratsea/repository/GameRepository.java
+++ src/main/java/com/enigma/waratsea/repository/GameRepository.java
@@ -1,7 +1,12 @@
 package com.enigma.waratsea.repository;
 
+import com.enigma.waratsea.model.Game;
+
 /**
  * Provides games.
  */
 public class GameRepository {
+  public void save(final Game game) {
+
+  }
 }
