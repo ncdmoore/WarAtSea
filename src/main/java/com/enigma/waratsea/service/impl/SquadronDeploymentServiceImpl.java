cc4c0f4e41d9391a7cc742d99b75efd857d7dfc0
David Moore <ncdmoore@gmail.com>
2022-12-28 10:47:36 -0500

    Change takeoff to takeOff

--- src/main/java/com/enigma/waratsea/service/impl/SquadronDeploymentServiceImpl.java
+++ src/main/java/com/enigma/waratsea/service/impl/SquadronDeploymentServiceImpl.java
@@ -5,14 +5,17 @@ import com.enigma.waratsea.event.LoadSquadronEvent;
 import com.enigma.waratsea.mapper.SquadronDeploymentMapper;
 import com.enigma.waratsea.model.Id;
 import com.enigma.waratsea.model.Side;
+import com.enigma.waratsea.model.squadron.Squadron;
 import com.enigma.waratsea.model.squadron.SquadronDeployment;
 import com.enigma.waratsea.repository.SquadronDeploymentRepository;
 import com.enigma.waratsea.service.SquadronDeploymentService;
+import com.enigma.waratsea.service.SquadronService;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,12 +23,15 @@ import java.util.stream.Collectors;
 @Singleton
 public class SquadronDeploymentServiceImpl implements SquadronDeploymentService {
   private final SquadronDeploymentRepository squadronDeploymentRepository;
+  private final SquadronService squadronService;
 
 
   @Inject
   public SquadronDeploymentServiceImpl(final Events events,
-                                       final SquadronDeploymentRepository squadronDeploymentRepository) {
+                                       final SquadronDeploymentRepository squadronDeploymentRepository,
+                                       final SquadronService squadronService) {
     this.squadronDeploymentRepository = squadronDeploymentRepository;
+    this.squadronService = squadronService;
 
     registerEvents(events);
   }
@@ -50,5 +56,11 @@ public class SquadronDeploymentServiceImpl implements SquadronDeploymentService 
   private void deploySquadron(final SquadronDeployment deployment) {
     log.info("airbase: '{}'", deployment.getAirbase());
     log.info("squadrons: '{}", deployment.getSquadrons().stream().map(Id::toString).collect(Collectors.joining(",")));
+
+    var airbase = deployment.getAirbase();
+    var squadronIds = new HashSet<>(deployment.getSquadrons());
+    var squadrons = squadronService.get(squadronIds);
+
+    log.info("Found squadrons: '{}'", squadrons.stream().map(Squadron::getId).map(Id::toString).collect(Collectors.joining(",")));
   }
 }
