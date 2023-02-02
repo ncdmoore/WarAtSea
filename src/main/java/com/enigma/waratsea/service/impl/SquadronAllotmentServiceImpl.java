7a044d20bdc974d2c919619740804262ad5ecf94
David Moore <ncdmoore@gmail.com>
2023-02-01 20:11:09 -0500

    Add squadron DeploymentState.

--- src/main/java/com/enigma/waratsea/service/impl/SquadronAllotmentServiceImpl.java
+++ src/main/java/com/enigma/waratsea/service/impl/SquadronAllotmentServiceImpl.java
@@ -22,10 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.enigma.waratsea.model.squadron.DeploymentState.NOT_DEPLOYED;
-import static com.enigma.waratsea.model.squadron.SquadronConfiguration.NONE;
-import static com.enigma.waratsea.model.squadron.SquadronState.CREATED;
-import static com.enigma.waratsea.model.squadron.SquadronStrength.FULL;
 import static java.util.stream.Collectors.toSet;
 
 @Slf4j
@@ -103,10 +99,6 @@ public class SquadronAllotmentServiceImpl implements SquadronAllotmentService {
     var entity = SquadronEntity.builder()
         .id(buildSquadronId(aircraftId))
         .aircraft(aircraftId)
-        .deploymentState(NOT_DEPLOYED)
-        .state(CREATED)
-        .configuration(NONE)
-        .strength(FULL)
         .build();
 
     return squadronMapper.toModel(entity);
