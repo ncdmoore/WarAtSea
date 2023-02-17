391b87b5d444fbc79eed70812dd62987d1ea5e8b
David Moore <ncdmoore@gmail.com>
2023-02-17 18:10:36 -0500

    Add mission repository junit

--- src/main/java/com/enigma/waratsea/mapper/VictoryMapper.java
+++ src/main/java/com/enigma/waratsea/mapper/VictoryMapper.java
@@ -1,14 +1,6 @@
 package com.enigma.waratsea.mapper;
 
-import com.enigma.waratsea.entity.matcher.BaseMatcherEntity;
-import com.enigma.waratsea.entity.matcher.EnemyMatcherEntity;
-import com.enigma.waratsea.entity.matcher.ShipMatcherEntity;
-import com.enigma.waratsea.entity.matcher.SquadronMatcherEntity;
 import com.enigma.waratsea.entity.victory.*;
-import com.enigma.waratsea.event.matcher.BaseMatcher;
-import com.enigma.waratsea.event.matcher.EnemyMatcher;
-import com.enigma.waratsea.event.matcher.ShipMatcher;
-import com.enigma.waratsea.event.matcher.SquadronMatcher;
 import com.enigma.waratsea.model.Id;
 import com.enigma.waratsea.model.port.Port;
 import com.enigma.waratsea.model.victory.*;
@@ -44,10 +36,6 @@ public abstract class VictoryMapper {
   @SubclassMapping(source = SquadronStepDestroyedVictoryEntity.class, target = SquadronStepDestroyedVictory.class)
   abstract public Victory toModel(final VictoryEntity victoryEntity);
 
-  abstract public ShipMatcher toShipMatcher(final ShipMatcherEntity shipMatcherEntity);
-  abstract public SquadronMatcher toSquadronMatcher(final SquadronMatcherEntity squadronMatcherEntity);
-  abstract public BaseMatcher toBaseMatcher(final BaseMatcherEntity baseMatcherEntity);
-  abstract public EnemyMatcher toEnemyMatcher(final EnemyMatcherEntity enemyMatcherEntity);
 
   @SubclassMapping(source = ShipBombardmentVictory.class, target = ShipBombardmentVictoryEntity.class)
   @SubclassMapping(source = ShipCargoLostVictory.class, target = ShipCargoLostVictoryEntity.class)
@@ -58,10 +46,6 @@ public abstract class VictoryMapper {
   @SubclassMapping(source = SquadronStepDestroyedVictory.class, target = SquadronStepDestroyedVictoryEntity.class)
   abstract public VictoryEntity toEntity(final Victory victory);
 
-  abstract public ShipMatcherEntity toShipMatcherEntity(final ShipMatcher combatMatcher);
-  abstract public SquadronMatcherEntity toSquadronMatcherEntity(final SquadronMatcher combatMatcher);
-  abstract public BaseMatcherEntity toBaseMatcherEntity(final BaseMatcher baseMatcher);
-  abstract public EnemyMatcherEntity toEnemyMatcherEntity(final EnemyMatcher enemyMatcher);
 
   Set<Port> mapPorts(final Set<Id> portIds) {
     return Optional.ofNullable(portIds)
