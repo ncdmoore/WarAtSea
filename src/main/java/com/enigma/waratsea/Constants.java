c771ae830f761983a2fa0d4ae3419408d35134a4
David Moore <ncdmoore@gmail.com>
2023-09-08 18:56:07 -0400

    Remove un-needed mappers.

--- src/main/java/com/enigma/waratsea/Constants.java
+++ src/main/java/com/enigma/waratsea/Constants.java
@@ -2,7 +2,6 @@ package com.enigma.waratsea;
 
 public final class Constants {
   public static final String ANY_SHIP = "ANY_SHIP";
-
   public static final String JSON_EXTENSION = ".json";
 
   private Constants() {
